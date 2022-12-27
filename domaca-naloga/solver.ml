b600acffd41880375e53123cc710200dcc5ed7cd
Lana Prijon <lana.prijon@gmail.com>
2022-12-27 11:15:48 +0100

    type state: add grid with available numbers

--- domaca-naloga/solver.ml
+++ domaca-naloga/solver.ml
@@ -2,7 +2,16 @@ type available = { loc : int * int; possible : int list }
 
 (* TODO: tip stanja ustrezno popravite, saj boste med reševanjem zaradi učinkovitosti
    želeli imeti še kakšno dodatno informacijo *)
-type state = { problem : Model.problem; current_grid : int option Model.grid }
+(* available_grid je tabela available tipov, ki vsebuje vse mmožne številke na posameznem mestu *)
+type state = { problem : Model.problem; current_grid : int option Model.grid; available_grid : int list Model.grid}
+
+let find_available (grid : int option Model.grid ) =
+  let available_list n =
+    match n with
+    | None -> [0; 1; 2; 3; 4; 5; 6; 7; 8; 9]
+    | Some n -> [n]
+  in
+  Model.map_grid available_list grid
 
 let print_state (state : state) : unit =
   Model.print_grid
@@ -12,7 +21,7 @@ let print_state (state : state) : unit =
 type response = Solved of Model.solution | Unsolved of state | Fail of state
 
 let initialize_state (problem : Model.problem) : state =
-  { current_grid = Model.copy_grid problem.initial_grid; problem }
+  { current_grid = Model.copy_grid problem.initial_grid; problem; available_grid = find_available problem.initial_grid }
 
 let validate_state (state : state) : response =
   let unsolved =
@@ -25,13 +34,25 @@ let validate_state (state : state) : response =
     if Model.is_valid_solution state.problem solution then Solved solution
     else Fail state
 
-let branch_state (state : state) : (state * state) option =
+
+let branch_state (state : state) =
   (* TODO: Pripravite funkcijo, ki v trenutnem stanju poišče hipotezo, glede katere
      se je treba odločiti. Če ta obstaja, stanje razveji na dve stanji:
      v prvem predpostavi, da hipoteza velja, v drugem pa ravno obratno.
      Če bo vaš algoritem najprej poizkusil prvo možnost, vam morda pri drugi
-     za začetek ni treba zapravljati preveč časa, saj ne bo nujno prišla v poštev. *)
-  failwith "TODO"
+     za začetek ni treba zapravljati preveč časa, saj ne bo nujno prišla v poštev. : (state * state) option *)
+  let state_1 = state in
+  let state_2 = state in
+  let rec choose_option grid i a b =
+    match grid.(a).(b) with
+    | x :: xs -> state_1.available_grid.(a).(b) <- x in state_2.available_grid.(a).(b) <- xs.(0)
+    | [x] when a <> 9 -> choose_option grid i (a + 1) b
+    | [x] when b <> 9 -> choose_option grid i+1 i (b + 1)
+    | else -> validate_state
+  in
+  choose_option state.available_grid 0 0 0
+  in
+  (state_1, state_2)
 
 (* pogledamo, če trenutno stanje vodi do rešitve *)
 let rec solve_state (state : state) =
